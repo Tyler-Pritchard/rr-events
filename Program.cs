632c744f4d490d1a34640f463060bf6cc31a7a29
Tyler Pritchard <pritchard.tyler@gmail.com>
2025-04-09 21:48:05 -0700

    create Tour page. fix a bug on the carousel. Update axios config for events service.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+using rr_events.Data;
+using rr_events.Controllers;
+using rr_events.Services;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ------------------------
+// 🔧 Service Configuration
+// ------------------------
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IEventQueryService, EventQueryService>();
+builder.Services.AddScoped<IEventCommandService, EventCommandService>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// Swagger (with XML comments)
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    c.IncludeXmlComments(xmlPath);
+});
+
+// ------------------------
+// ⚙️ App Pipeline Configuration
+// ------------------------
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
+public partial class Program { }
